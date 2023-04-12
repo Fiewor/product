cd33d6d296f4e3742e4a68107d352c5e61697db0
Fiewor <johnfiewor@gmail.com>
2023-04-12 11:41:55 +0100

    refactor: change to "data router"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
@@ -7,18 +7,36 @@ import Error from "./pages/Error";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Form from "./pages/Form/Form";
+import Product from "./pages/Product/Product";
+import { useGetSingleProductQuery } from "./services/product";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "products/:id",
+    element: <Product />,
+  },
+  {
+    path: "form",
+    element: <Form />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,13 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
