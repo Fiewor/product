f625b057b1194ab546ebb5a95314242cdaefbd3f
Fiewor <johnfiewor@gmail.com>
2023-04-12 11:43:21 +0100

    refatctor: display search only in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import "./home.scss";
 import { allProducts } from "../../features/products/productSlice";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import ProductList from "../ProductList/ProductList";
+import Header from "../../components/Header/Header";
 
 const Home = () => {
   const dispatch = useAppDispatch();
@@ -17,6 +18,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      <Header />
       <ProductList />
     </div>
   );
